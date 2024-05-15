137f359fc3c497cb2bef343db47e6acb744ee807
tuna <fusornat@gmail.com>
2024-05-15 11:11:16 +0700

    done chua bat loi thoat form

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -131,7 +131,6 @@
 
     @include('customer.components.note')
     @include('customer.components.remind')
-    @include('customer.modal.add_card')
     @include('customer.modal.add')
     @include('customer.modal.edit')
     @include('customer.modal.editCard')

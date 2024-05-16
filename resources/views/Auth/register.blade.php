3c6f2d814adc72ef236f7fc53dfd438865d8e948
Tran Binh <binhcoder02@gmail.com>
2024-05-16 14:44:09 +0700

    .

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -1,4 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+@section('title')
+    Đăng ký
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">

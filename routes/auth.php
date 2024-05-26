455bb18b542ce493287e153c5159ba44ac1550e0
Tran Binh <binhcoder02@gmail.com>
2024-05-26 19:24:45 +0700

    fix

--- routes/auth.php
+++ routes/auth.php
@@ -22,7 +22,7 @@ Route::middleware(["auth"])->group(function () {
 // Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 // Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 // Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+// Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 // Email Verification Routes
 // Route::get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');

2a4aa9392f6f24130304067159a57e0138dfb3ac
Tran Binh <binhcoder02@gmail.com>
2024-11-20 23:35:08 +0700

    update

--- pull_code.sh
+++ pull_code.sh
@@ -1,27 +1,17 @@
-#!/bin/bash
-
 # Đường dẫn đến repo GitHub
 REPO_URL="https://github.com/binh-dev-k2/cong_no.git"
 BRANCH="live" # Nhánh cần pull (sửa nếu cần)
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-# Chuyển vào thư mục của script
-cd "$SCRIPT_DIR" || exit
-
+cd "$(dirname "$0")"
+sed -i -e 's/\r$//' pull_code.sh
 # Pull code mới nhất
 echo "Pulling the latest code from $REPO_URL..."
 git pull origin $BRANCH
-
-
 # Cài đặt các dependencies (composer)
 echo "Installing PHP dependencies..."
 composer install --no-dev --optimize-autoloader
-
 # Chạy migration (nếu cần)
 echo "Running database migrations..."
 php artisan migrate --force
-
 # Dọn dẹp cache (nếu cần)
 echo "Clearing and optimizing cache..."
 php artisan o:c
@@ -29,5 +19,4 @@ php artisan config:clear
 php artisan config:cache
 php artisan route:clear
 php artisan route:cache
-
 echo ">>>>>>>Done!"

88e9fcd7e21937c3974f8a390236ac9efab8a394
Tran Binh <binhcoder02@gmail.com>
2024-11-22 21:52:49 +0700

    update

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,7 +4,7 @@
         <!--begin::Copyright-->
         <div class="text-gray-900 order-2 order-md-1">
             <span class="text-muted fw-semibold me-1">2024&copy;</span>
-            <a href="#" target="_blank" class="text-gray-800 text-hover-primary">Dungx Mokahd</a>
+            <a href="#" class="text-gray-800 text-hover-primary">Dungx Mokahd</a>
         </div>
         <!--end::Copyright-->
         <!--begin::Menu-->

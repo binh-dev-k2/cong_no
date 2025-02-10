cfbf9903d3eef6968b7a5ca8c9db006fb5deeba4
Tran Binh <binhcoder02@gmail.com>
2025-02-10 12:51:36 +0700

    update

--- pull_code.sh
+++ pull_code.sh
@@ -1,9 +1,9 @@
 # Đường dẫn đến repo GitHub
 REPO_URL="https://github.com/binh-dev-k2/cong_no.git"
-BRANCH="live" # Nhánh cần pull (sửa nếu cần)
+BRANCH="live" 
 cd "$(dirname "$0")"
 sed -i -e 's/\r$//' pull_code.sh
-# Pull code mới nhất
+
 git clone --branch $BRANCH --single-branch $REPO_URL temp_folder
 rsync -a temp_folder/ . --remove-source-files
 rm -rf temp_folder
@@ -12,13 +12,12 @@ rm -f ./config/l5-swagger.php
 
 php artisan o:c
 
-# Cài đặt các dependencies (composer)
 rm -rf vendor
 rm -f composer.lock
 composer install --no-dev --optimize-autoloader
-# Chạy migration (nếu cần)
+
 php artisan migrate --force
 php artisan db:seed
-# Dọn dẹp cache (nếu cần)
+
 php artisan o:c
 echo ">>>>>>>Done!"

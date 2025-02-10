682f37eed82e2ccb645f64057b35b139a294956b
Tran Binh <binhcoder02@gmail.com>
2025-02-10 12:50:14 +0700

    update

--- pull_code.sh
+++ pull_code.sh
@@ -4,7 +4,6 @@ BRANCH="live" # Nhánh cần pull (sửa nếu cần)
 cd "$(dirname "$0")"
 sed -i -e 's/\r$//' pull_code.sh
 # Pull code mới nhất
-echo "Pulling the latest code from $REPO_URL..."
 git clone --branch $BRANCH --single-branch $REPO_URL temp_folder
 rsync -a temp_folder/ . --remove-source-files
 rm -rf temp_folder
@@ -14,15 +13,12 @@ rm -rf ./config/l5-swagger.php
 php artisan o:c
 
 # Cài đặt các dependencies (composer)
-echo "Installing PHP dependencies..."
 rm -rf vendor
 rm -f composer.lock
 composer install --no-dev --optimize-autoloader
 # Chạy migration (nếu cần)
-echo "Running database migrations..."
 php artisan migrate --force
 php artisan db:seed
 # Dọn dẹp cache (nếu cần)
-echo "Clearing and optimizing cache..."
 php artisan o:c
 echo ">>>>>>>Done!"
